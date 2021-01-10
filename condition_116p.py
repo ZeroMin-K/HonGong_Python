d5eadca6a672628fa9c166f33ed57e2901a8a0ce
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-01-10 21:41:50 +0900

    chpater03

---
+++ condition_116p.py
@@ -0,0 +1,15 @@
+# input
+number = input("input integer> ")
+number = int(number)
+
+# positive condition
+if number > 0:
+    print("It is positive")
+
+# negative condition
+if number < 0:
+    print("it is negative")
+
+# zero condition
+if number == 0:
+    print("it is 0")
