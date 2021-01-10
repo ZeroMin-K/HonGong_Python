6e3edf059131e86c8190aa8f1df10e79f3bf5169
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-01-10 21:37:56 +0900

    chapter01

---
+++ output.py
@@ -0,0 +1,17 @@
+# print one thing
+print("# print one thing")
+print("Hello Python Programming...!")
+print()
+
+# print many things
+print("# print many things")
+print(10, 20, 30, 40, 50)
+print("Hello", "my", "name", "is","Kim")
+print()
+
+# if not input, just new line
+print("# print nothing")
+print("---checking line---")
+print()
+print()
+print("---checking line---")
