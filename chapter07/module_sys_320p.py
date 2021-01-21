1f057c43d1522884c419b43e3dd0f8e162fb44c4
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-01-21 22:04:50 +0900

    chapter07 실습 예제 코드

---
+++ chapter07/module_sys_320p.py
@@ -0,0 +1,9 @@
+import sys
+
+print(sys.argv)
+print("---")
+
+print("getwindowsversion():", sys.getwindowsversion())
+print("copyright:", sys.copyright)
+print("version:", sys.version)
+sys.exit() 
---
+++ chapter07/module_time_326p.py
@@ -0,0 +1,4 @@
+import time
+print("stop 5 second")
+time.sleep(5)
+print("program exit")
